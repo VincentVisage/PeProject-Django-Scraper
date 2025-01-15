9630a89b23dd4ebd904b05caadfc07fcdadf4189
VincentVisage <144054905+VincentVisage@users.noreply.github.com>
2025-01-15 04:33:52 +0300

    scrape func

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,11 +5,14 @@ from bs4 import BeautifulSoup
 # Create your views here.
 
 
-def scrape(requests):
+# Scrapes links from page
+def scrape(requests):                              
     page = requests.get('https://www.google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
-
+    
     link_address = []
 
     for link in soup.find_all('a'):
         link_address.append(link.get('href'))
+
+    return render(request, 'myapp/result.html', {'link_address':link_address})
