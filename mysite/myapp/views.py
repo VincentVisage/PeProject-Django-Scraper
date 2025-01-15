342dada59f1db2ef28037bfc1d35bba5f769073d
VincentVisage <144054905+VincentVisage@users.noreply.github.com>
2025-01-15 04:53:46 +0300

    added template and model

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from .models import Link
 
 # Create your views here.
 
 
 # Scrapes links from page
-def scrape(requests):                              
+def scrape(request):                              
     page = requests.get('https://www.google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
-    
-    link_address = []
 
     for link in soup.find_all('a'):
-        link_address.append(link.get('href'))
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name=link_text)
 
-    return render(request, 'myapp/result.html', {'link_address':link_address})
+    data = Link.objects.all()
+
+    return render(request, 'myapp/result.html', {'data': data})
